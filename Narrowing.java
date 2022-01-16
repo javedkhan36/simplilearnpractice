c3d56b96dbc4143404e2904bd65955f2fa01321f
javedkhan36 <97622311+javedkhan36@users.noreply.github.com>
2022-01-16 20:11:43 +0530

    Add files via upload

---
+++ Narrowing.java
@@ -0,0 +1,29 @@
+package com.simplilearn.typecasting;
+
+public class Narrowing {
+	
+public static void main(String[] args) {
+		
+		double j = 166.66;
+		System.out.println(j);
+		
+		
+		System.out.println("double to long:");
+		long k = (long)j;
+		System.out.println(k);
+		
+
+		System.out.println("long to int:");
+		int a = (int)k;
+		System.out.println(a);
+		
+		
+		char a1 = 'A';
+		int b = a1;
+		System.out.println(b);
+		
+	
+				
+	}
+
+}
---
+++ Widening.java
@@ -0,0 +1,22 @@
+package com.simplilearn.typecasting;
+
+public class Widening {
+	
+	public static void main(String[] args) {
+		
+		int a = 7;
+		
+		long b = a;
+		
+		float c = b;
+
+		
+		System.out.println(a);
+		System.out.println("int to long:");
+		System.out.println(b);
+		System.out.println("long to float:");
+		System.out.println(c);
+		
+	}
+
+}
